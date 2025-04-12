2548617757e6d6b089ca56f0b8c6d699832b8867
Darya Sushkova <dasha.030105@gmail.com>
2025-04-12 17:34:18 +0300

    feat: add main icons components
    QW-89

---
+++ assets/icons/types.ts
@@ -0,0 +1,11 @@
+import { GestureResponderEvent, StyleProp, ViewStyle } from "react-native";
+
+export interface IIcon {
+  style?: StyleProp<ViewStyle>;
+  color?: string;
+  height?: number;
+  width?: number;
+  opacity?: number;
+  onClick?: (event: GestureResponderEvent) => void;
+  stroke?: number;
+}
