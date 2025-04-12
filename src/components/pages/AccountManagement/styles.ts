6979957ac32d5d2bf68ff58fa28221dedabff0c2
Darya Sushkova <dasha.030105@gmail.com>
2025-04-12 19:48:06 +0300

    feat: add account control page component
    QW-90

---
+++ src/components/pages/AccountManagement/styles.ts
@@ -0,0 +1,88 @@
+import { StyleSheet } from "react-native";
+import { palette } from "../../../constants/palette";
+import { fonts } from "../../../constants/fonts";
+
+export const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    padding: 28,
+    alignItems: "center",
+  },
+  managerWrapper: {
+    marginTop: 36,
+    paddingHorizontal: 6,
+    width: "100%",
+    alignItems: "center",
+  },
+  accordion: {
+    width: "100%",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 20,
+    paddingVertical: 10,
+  },
+  headerLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  headerText: {
+    color: palette.mainText,
+    fontFamily: fonts.semibold,
+  },
+  content: {
+    backgroundColor: palette.folderOrHighlightedSectionBg,
+    borderRadius: 10,
+    padding: 8,
+  },
+  input: {
+    backgroundColor: palette.textFieldInFolderBg,
+    borderRadius: 6,
+    paddingHorizontal: 8,
+    height: 20,
+    color: palette.mainText,
+    fontFamily: fonts.semibold,
+    marginBottom: 4,
+    width: "60%",
+  },
+  inputLabel: {
+    color: palette.labelTransparentText,
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  dropdownWrapper: {
+    width: "60%",
+  },
+  dropdownLabel: {
+    color: palette.labelTransparentText,
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  dropdown: {
+    backgroundColor: palette.textFieldInFolderBg,
+    height: 20,
+  },
+  itemContainerStyle: {
+    paddingVertical: 8,
+  },
+  btns: {
+    width: "100%",
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    gap: 8,
+  },
+  btn: {
+    width: "28%",
+    height: 20,
+    borderRadius: 6,
+  },
+  btnText: {
+    color: palette.subTextMainScreenPopup,
+    fontSize: 12,
+    lineHeight: 16,
+    fontFamily: fonts.semibold,
+  },
+});
---
+++ src/components/pages/AccountManagement/types.ts
@@ -0,0 +1,7 @@
+export interface IUserSection {
+  id: string;
+  title: string;
+  login: string;
+  password: string;
+  role: string;
+}
