e888c54a6c28f9aa46e57d5b9263bf8005ed5f3c
Darya Sushkova <dasha.030105@gmail.com>
2025-04-12 19:15:28 +0300

    feat: add radio atom component
    QW-72

---
+++ src/components/atoms/Radio/Radio.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from "react";
+import { Pressable, Text, View } from "react-native";
+import { IRadio } from "./types";
+import { styles } from "./styles";
+
+const Radio: FC<IRadio> = ({
+  label,
+  isChecked = false,
+  setIsChecked,
+  style,
+}) => (
+  <Pressable onPress={setIsChecked} style={[styles.wrapper, style]}>
+    <View style={styles.radioWrapper}>
+      <View style={styles.radio}>
+        {isChecked && <View style={styles.radioChecked} />}
+      </View>
+      <Text style={styles.radioText}>{label}</Text>
+    </View>
+  </Pressable>
+);
+
+export default Radio;
---
+++ src/components/atoms/Radio/styles.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+import { palette } from "../../../constants/palette";
+
+export const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  radioWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  radio: {
+    backgroundColor: palette.blue,
+    borderRadius: "50%",
+    width: 18,
+    height: 18,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  radioChecked: {
+    backgroundColor: palette.brightBlue,
+    borderRadius: "50%",
+    width: 10,
+    height: 10,
+  },
+  radioText: {
+    color: palette.mainText,
+    fontSize: 20,
+    lineHeight: 27,
+  },
+});
