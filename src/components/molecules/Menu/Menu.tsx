f960714a9b24b02d623e429ecad8d811c165f165
Darya Sushkova <dasha.030105@gmail.com>
2025-04-13 13:49:17 +0300

    feat: add menu molecule component
    QW-75

---
+++ src/components/molecules/Menu/Menu.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { menuItems } from "../../../constants/menuItems";
+import { Pressable, Text, View } from "react-native";
+import MenuItem from "../../atoms/MenuItem";
+import { IMenuItem } from "./types";
+import { styles } from "./styles";
+import { LogoIcon, MenuProfileIcon } from "../../../../assets/icons";
+import { useMainNavigation } from "../../../hooks/useTypedNavigation";
+import Hamburger from "../../atoms/Hamburger/Hamburger";
+
+const Menu = () => {
+  const { navigate } = useMainNavigation();
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const toggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <View style={[styles.wrapper]}>
+      <View style={styles.header}>
+        <Pressable style={[styles.headerBtn, { width: 44 }]} onPress={toggle}>
+          <Hamburger active={isExpanded} />
+          <Text style={styles.headerBtnTxt}>
+            {isExpanded ? "Скрыть" : "Меню"}
+          </Text>
+        </Pressable>
+        <LogoIcon width={83} />
+        <Pressable style={styles.headerBtn} onPress={() => navigate("Profile")}>
+          <MenuProfileIcon />
+          <Text style={styles.headerBtnTxt}>Профиль</Text>
+        </Pressable>
+      </View>
+      {menuItems.map((item: IMenuItem, index: number) => (
+        <MenuItem
+          key={index}
+          index={index}
+          icon={item.icon}
+          title={item.title}
+          isRoundBorder={index === menuItems.length - 1}
+          isExpanded={isExpanded}
+          onPress={() => navigate(item.path)}
+        />
+      ))}
+    </View>
+  );
+};
+
+export default Menu;
---
+++ src/components/molecules/Menu/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+import { palette } from "../../../constants/palette";
+
+export const styles = StyleSheet.create({
+  wrapper: {
+    width: "100%",
+    zIndex: 100,
+    backgroundColor: palette.blue2,
+  },
+  header: {
+    zIndex: 100,
+    height: 73,
+    width: "100%",
+    backgroundColor: palette.blue2,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+  },
+  headerBtn: {
+    alignItems: "center",
+    gap: 2,
+  },
+  headerBtnTxt: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: palette.mainText,
+  },
+});
