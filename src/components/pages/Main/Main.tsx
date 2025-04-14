75992a9b1891e91bd85b22aa1496a94a80411b16
Darya Sushkova <dasha.030105@gmail.com>
2025-04-14 09:54:36 +0300

    feat: add main accordion on main page
    QW-75

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -1,12 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTemplate from "../../templates/PageTemplate";
-import { View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { styles } from "./styles";
+import Accordion from "react-native-collapsible/Accordion";
+import { cameras } from "../../../constants/cameras";
+import { ArrowTopFAQIcon, SearchIcon } from "../../../../assets/icons";
+import Input from "../../atoms/Input";
+import { palette } from "../../../constants/palette";
 
 const Main = () => {
+  const [activeSections, setActiveSections] = useState<number[]>([]);
+  const [sectionsOnline, setSectionsOnline] = useState<number[]>([]);
+  const [sectionsOffline, setSectionsOffline] = useState<number[]>([]);
+
+  const onlineCameras = cameras.filter((camera) => camera.online);
+  const offlineCameras = cameras.filter((camera) => !camera.online);
+
+  const sections = [
+    { title: `Online (${onlineCameras.length})`, cameras: onlineCameras },
+    { title: `Offline (${offlineCameras.length})`, cameras: offlineCameras },
+  ];
+
+  const renderHeader = (section: (typeof sections)[number], index: number) => (
+    <View style={styles.header}>
+      <View style={styles.headerSearch}>
+        <Text style={styles.title}>{section.title}</Text>
+        {section.cameras.length > 0 && (
+          <Input
+            placeholder="Поиск..."
+            placeholderTextColor={palette.mainText}
+            customStyles={styles.input}
+            customInputStyles={styles.customInputStyles}
+            rightIcon={<SearchIcon />}
+          />
+        )}
+      </View>
+      <ArrowTopFAQIcon
+        style={activeSections[0] !== index && { transform: [{ scaleY: -1 }] }}
+      />
+    </View>
+  );
+
+  const renderContent = (section: (typeof sections)[number]) => (
+    <View>
+      <Text>{section.title}</Text>
+    </View>
+  );
+
   return (
     <PageTemplate hasMenu>
-      <View style={styles.wrapper}></View>
+      <View style={styles.wrapper}>
+        <Accordion
+          sections={sections}
+          activeSections={activeSections}
+          renderHeader={renderHeader}
+          renderContent={renderContent}
+          onChange={setActiveSections}
+          touchableComponent={Pressable}
+        />
+      </View>
     </PageTemplate>
   );
 };
--- src/components/pages/Main/styles.ts
+++ src/components/pages/Main/styles.ts
@@ -1,9 +1,39 @@
 import { StyleSheet } from "react-native";
 import { palette } from "../../../constants/palette";
+import { fonts } from "../../../constants/fonts";
 
 export const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     backgroundColor: palette.bg,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: palette.mainOnlineOfflineBg,
+    padding: 16,
+    gap: 70,
+    alignItems: "center",
+  },
+  headerSearch: {
+    flexDirection: "row",
+    gap: 36,
+    alignItems: "center",
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontFamily: fonts.semibold,
+    color: palette.mainText,
+  },
+  input: {
+    flex: 1,
+  },
+  customInputStyles: {
+    height: 22,
+    backgroundColor: palette.searchBarBg,
+    paddingRight: 28,
+    color: palette.mainText,
   },
 });
