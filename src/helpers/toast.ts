814f7ccf93c22a3a5c7617f1a29a753729728b8c
Darya Sushkova <dasha.030105@gmail.com>
2025-05-03 12:03:13 +0300

    fix: change toasts library
    QW-125

--- src/helpers/toast.ts
+++ src/helpers/toast.ts
@@ -1,57 +1,19 @@
-import { toast, ToastOptions } from "@backpackapp-io/react-native-toast";
-import { palette } from "../constants/palette";
-import { fonts } from "../constants/fonts";
-import { TextStyle, ViewStyle } from "react-native";
+import Toast, { ToastType } from "react-native-toast-message";
 
-const MAX_TOASTS = 2;
-let activeToastIds: string[] = [];
-
-const baseToastStyles = {
-  indicator: { backgroundColor: palette.mainText },
-  text: {
-    color: palette.mainText,
-    fontFamily: fonts.bold,
-    fontSize: 16,
-  } as TextStyle,
-};
-
-const handleToast = (
-  type: "success" | "error",
-  message: string,
-  backgroundColor: string,
-  options?: ToastOptions
-) => {
-  const duration = options?.duration ?? 1400;
-
-  if (activeToastIds.length >= MAX_TOASTS) {
-    const oldestId = activeToastIds.shift();
-    toast.dismiss(oldestId);
-  }
-
-  const id = toast[type](message, {
-    duration,
-    styles: {
-      ...baseToastStyles,
-      pressable: {
-        backgroundColor,
-        borderRadius: 12,
-      } as ViewStyle,
-    },
-    ...options,
+const showToast = (type: ToastType, message: string, duration = 1400) => {
+  Toast.show({
+    type,
+    text1: message,
+    visibilityTime: duration,
+    autoHide: true,
   });
-
-  activeToastIds.push(id);
-
-  setTimeout(() => {
-    activeToastIds = activeToastIds.filter((tId) => tId !== id);
-  }, duration);
 };
 
-export const showSuccessToast = (msg: string, opts?: ToastOptions) =>
-  handleToast("success", msg, palette.greenOnline, opts);
+export const showSuccessToast = (msg: string, duration?: number) =>
+  showToast("success", msg, duration);
 
-export const showErrorToast = (msg: string, opts?: ToastOptions) =>
-  handleToast("error", msg, palette.red, opts);
+export const showErrorToast = (msg: string, duration?: number) =>
+  showToast("error", msg, duration);
 
-export const showInfoToast = (msg: string, opts?: ToastOptions) =>
-  handleToast("success", msg, palette.info, opts);
+export const showInfoToast = (msg: string, duration?: number) =>
+  showToast("info", msg, duration);
